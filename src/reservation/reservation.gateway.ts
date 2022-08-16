17a20538b16e77cd0c73aed848c2645d0edeab07
Bernie <itdalde@gmail.com>
2022-08-17 07:59:52 +0800

    Fix

--- src/reservation/reservation.gateway.ts
+++ src/reservation/reservation.gateway.ts
@@ -1,7 +1,7 @@
 import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
-@WebSocketGateway({ namespace: '/status' })
+@WebSocketGateway({ namespace: '/reservation' })
 export class ReservationGateway {
 
   @WebSocketServer() wss: Server;
